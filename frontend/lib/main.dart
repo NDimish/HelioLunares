caa25145a98089204c44601a8d7056f577254a06
Israfeel Ashraf <israfeel.ashraf@kcl.ac.uk>
2023-01-31 20:23:57 +0000

    Add basic landing page with buttons to start authentication

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,19 +1,73 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const UniversityTicketingSystem());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class UniversityTicketingSystem extends StatelessWidget {
+  const UniversityTicketingSystem({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'University Ticketing System',
+      theme: ThemeData(useMaterial3: true),
+      debugShowCheckedModeBanner: false,
+      home: const LandingPage(),
+    );
+  }
+}
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text("University Ticketing System"),
+            leading: const Icon(Icons.airplane_ticket_outlined, size: 30)),
+        body: Center(
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+              const Text("This page must be styled"),
+              /*Maybe use an icon like the airplane ticket*/
+              const SizedBox(height: 40),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  ElevatedButton(
+                      onPressed: moveToLogInPage,
+                      style: ElevatedButton.styleFrom(
+                        shadowColor: Colors.blueAccent,
+                        elevation: 3,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(32.0)),
+                        minimumSize: const Size(200, 60), //////// HERE
+                      ),
+                      child: const Text("Log In")),
+                  const SizedBox(width: 40),
+                  ElevatedButton(
+                      onPressed: moveToSignUpPage,
+                      style: ElevatedButton.styleFrom(
+                        shadowColor: Colors.blueAccent,
+                        elevation: 3,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(32.0)),
+                        minimumSize: const Size(200, 60), //////// HERE
+                      ),
+                      child: const Text("Sign Up"))
+                ],
+              ),
+            ])));
+  }
+
+  void moveToLogInPage() {
+    print("Redirect to log in page");
+  }
+
+  void moveToSignUpPage() {
+    print("Redirect to sign up page");
   }
 }
