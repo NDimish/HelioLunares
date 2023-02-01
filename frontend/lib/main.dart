db84145cd54754eee6df3ce8bf0fca507e024e0f
Israfeel Ashraf <israfeel.ashraf@kcl.ac.uk>
2023-02-01 19:21:20 +0000

    Add text fields to the society sign up page

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -120,13 +120,115 @@ class SignUpAsSocietyScreen extends StatefulWidget {
 }
 
 class _SignUpAsSocietyScreenState extends State<SignUpAsSocietyScreen> {
+  TextEditingController _textControllerEmail = TextEditingController();
+  TextEditingController _textControllerPassword = TextEditingController();
+
+  TextEditingController _societyName = TextEditingController();
+  TextEditingController _creationDate = TextEditingController();
+
+  TextEditingController _uniStudyingAt = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Sign up as a society")),
-      body:
-          const Center(child: Text("This is the sign up screen for societies")),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.75,
+            child: TextField(
+              controller: _textControllerEmail,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                hintStyle: const TextStyle(color: Colors.blue),
+                prefixIcon: const Icon(Icons.email_outlined),
+                hintText: "Enter Email",
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.75,
+            child: TextField(
+              controller: _textControllerPassword,
+              keyboardType: TextInputType.visiblePassword,
+              decoration: InputDecoration(
+                hintStyle: const TextStyle(color: Colors.blue),
+                prefixIcon: const Icon(Icons.lock_outline),
+                hintText: "Enter Password",
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.75,
+            child: TextField(
+              controller: _textControllerPassword,
+              keyboardType: TextInputType.name,
+              decoration: InputDecoration(
+                hintStyle: const TextStyle(color: Colors.blue),
+                prefixIcon: const Icon(Icons.lock_outline),
+                hintText: "Enter Society Name",
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.75,
+            child: TextField(
+              controller: _uniStudyingAt,
+              keyboardType: TextInputType.name,
+              decoration: InputDecoration(
+                hintStyle: const TextStyle(color: Colors.blue),
+                prefixIcon: const Icon(Icons.school_outlined),
+                hintText: "Enter University You Are Studying At",
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.75,
+            child: TextField(
+              controller: _creationDate,
+              keyboardType: TextInputType.datetime,
+              decoration: InputDecoration(
+                hintStyle: const TextStyle(color: Colors.blue),
+                prefixIcon: const Icon(Icons.date_range_outlined),
+                hintText: "Enter Society Creation Date",
+                border:
+                    OutlineInputBorder(borderRadius: BorderRadius.circular(15)),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          ElevatedButton(
+              onPressed: signUpSocietyAndRedirect,
+              style: ElevatedButton.styleFrom(
+                shadowColor: Colors.blueAccent,
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16.0)),
+                minimumSize: const Size(180, 60), //////// HERE
+              ),
+              child: const Text("Sign Up")),
+        ],
+      )),
     );
+  }
+
+  void signUpSocietyAndRedirect() {
+    print("Society successfully signed up");
+    print("Redirecting...");
   }
 }
 
@@ -217,7 +319,7 @@ class _SignUpAsStudentScreenState extends State<SignUpAsStudentScreen> {
             SizedBox(
               width: MediaQuery.of(context).size.width * 0.75,
               child: TextField(
-                controller: _firstName,
+                controller: _uniStudyingAt,
                 keyboardType: TextInputType.name,
                 decoration: InputDecoration(
                   hintStyle: const TextStyle(color: Colors.blue),
@@ -253,7 +355,7 @@ class _SignUpAsStudentScreenState extends State<SignUpAsStudentScreen> {
                       borderRadius: BorderRadius.circular(16.0)),
                   minimumSize: const Size(180, 60), //////// HERE
                 ),
-                child: const Text("Log In")),
+                child: const Text("Sign Up In")),
           ],
         ),
       ),
