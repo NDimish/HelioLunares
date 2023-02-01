974ffe356ab06acb4d11dcb3a6ab46b979cd6201
Israfeel Ashraf <israfeel.ashraf@kcl.ac.uk>
2023-02-01 12:21:43 +0000

    Add log in page functionality

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -117,11 +117,65 @@ class LogInScreen extends StatefulWidget {
 }
 
 class _LogInScreenState extends State<LogInScreen> {
+  TextEditingController _textControllerEmail = TextEditingController();
+  TextEditingController _textControllerPassword = TextEditingController();
+
+  bool isUserLoggedIn = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Log in")),
-      body: const Center(child: Text("This is the log in screen")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.75,
+              child: TextField(
+                controller: _textControllerEmail,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintStyle: const TextStyle(color: Colors.blue),
+                  prefixIcon: const Icon(Icons.email_outlined),
+                  hintText: "Enter Email",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.75,
+              child: TextField(
+                controller: _textControllerPassword,
+                keyboardType: TextInputType.visiblePassword,
+                decoration: InputDecoration(
+                  hintStyle: const TextStyle(color: Colors.blue),
+                  prefixIcon: const Icon(Icons.lock_outline),
+                  hintText: "Enter Password",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50)),
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: logUserIn,
+                style: ElevatedButton.styleFrom(
+                  shadowColor: Colors.blueAccent,
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(32.0)),
+                  minimumSize: const Size(180, 60), //////// HERE
+                ),
+                child: const Text("Log In")),
+          ],
+        ),
+      ),
     );
+  }
+
+  void logUserIn() {
+    print("This method will send the post request with the users details.");
   }
 }
