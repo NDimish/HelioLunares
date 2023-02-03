96e803f080eaf33175346b0fb7f63193bb6d56c6
Josh <joshuasusak@gmail.com>
2023-02-03 12:18:23 +0000

    Add route to user settings page
    - For testing purposes, app begins on settings page

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/cupertino.dart';
+import 'package:university_ticketing_system/screens/user_settings/user_settings.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +15,13 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'User Settings',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      initialRoute: '/',
+      routes: {'/': (context) => const UserSettingsPage()},
+    );
   }
 }
