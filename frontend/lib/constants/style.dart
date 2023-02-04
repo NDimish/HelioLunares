1f28555993665c5ae2aa4f1097e2f4ef5388bf0c
SitaSeth <k21034097@kcl.ac.uk>
2023-02-04 20:26:17 +0000

    Create style file and specify theme colours

---
+++ frontend/lib/constants/style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+//Theme Colours of app
+Color light = Color(0XFFBAADC1);
+Color lightGrey = Color(0XFFfcfcfc);
+Color active = Color(0XFF898aa7);
+Color dark = Color(0x00000143c57);
