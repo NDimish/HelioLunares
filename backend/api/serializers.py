f7a629e66c3d2043c1febc19f9dcc035d3e72271
Israfeel Ashraf <israfeel.ashraf@kcl.ac.uk>
2023-02-04 14:44:42 +0000

    Add authentication for logging user in and out, but not sign up

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,1 +1,17 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from api.models import Student, User, University
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = '__all__'
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        
+class UniversitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = University
+        fields = '__all__'
