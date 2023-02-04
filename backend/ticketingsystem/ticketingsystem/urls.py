9b48fd77cedce47d7a4756a3f56a247e9ea62e98
Jaidev Nileshkumar K21054236 <jaidev.nileshkumar@kcl.ac.uk>
2023-02-04 13:05:43 +0000

    working template

--- backend/ticketingsystem/ticketingsystem/urls.py
+++ backend/ticketingsystem/ticketingsystem/urls.py
@@ -13,16 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-from ticketing_api import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', include('ticketing_api.urls')),
 ]
