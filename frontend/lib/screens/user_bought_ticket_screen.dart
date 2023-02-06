3419727bab5d171a1011bca34c06665f229da9a3
Isaac Leegit config --global user.name Isaac <shun.s.lee@kcl.ac.uk>
2023-02-06 12:35:20 +0000

    - Create a user_nought_ticket_screen

---
+++ frontend/lib/screens/user_bought_ticket_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserBoughtTicketScreen extends StatefulWidget {
+  const UserBoughtTicketScreen({super.key});
+
+  @override
+  State<UserBoughtTicketScreen> createState() => _UserBoughtTicketScreenState();
+}
+
+class _UserBoughtTicketScreenState extends State<UserBoughtTicketScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+} 
