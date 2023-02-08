576420121a0bce570c77c79b1a80d23d47e6e9f3
Josh <joshuasusak@gmail.com>
2023-02-08 14:17:58 +0000

    Add social icons to startup screen and change bg

--- frontend/lib/screens/startup_screen/startup_screen.dart
+++ frontend/lib/screens/startup_screen/startup_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:university_ticketing_system/screens/startup_screen/widgets/social_icons.dart';
 import 'package:university_ticketing_system/screens/startup_screen/widgets/startup_footer.dart';
 import 'package:university_ticketing_system/screens/startup_screen/widgets/startup_navbar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class StartupScreen extends StatefulWidget {
   const StartupScreen({super.key});
@@ -15,15 +17,27 @@ class _StartupScreenState extends State<StartupScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red,
+      backgroundColor: Color.fromRGBO(251, 247, 245, 1),
       appBar: const StartupNavbar(),
       body: SafeArea(
           child: Center(
               child: Column(children: [
-        Flexible(child: const Text("HERO")),
-        Flexible(child: const Text("ABOUT")),
-        Flexible(child: const Text("HOW TO USE")),
-        Flexible(child: const Text("CONTACT"))
+        Flexible(child: Text("HERO")),
+        Row(
+          children: const [
+            SocialIcon(
+              icon: Icon(Icons.play_arrow_sharp),
+            ),
+            SocialIcon(
+              icon: Icon(
+                Icons.apple,
+              ),
+            ),
+          ],
+        ),
+        Flexible(child: Text("ABOUT")),
+        Flexible(child: Text("HOW TO USE (Will be on separate page)")),
+        Flexible(child: Text("CONTACT (Will be on separate page)"))
       ]))),
       bottomNavigationBar: const Footer(),
     );
