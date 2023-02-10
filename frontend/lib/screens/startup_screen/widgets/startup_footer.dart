5b97d3f28f29b8b6a739fa3c7c1c7825c0b6a447
Josh <joshuasusak@gmail.com>
2023-02-10 14:56:43 +0000

    Add further styling to footer

--- frontend/lib/screens/startup_screen/widgets/startup_footer.dart
+++ frontend/lib/screens/startup_screen/widgets/startup_footer.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
+
+TextSpan footerText(message) {
+  return TextSpan(
+      text: message, style: GoogleFonts.barlow(color: Colors.white));
+}
 
 class Footer extends StatelessWidget {
   const Footer({super.key});
@@ -10,6 +16,7 @@ class Footer extends StatelessWidget {
     return Container(
         height: 50,
         child: BottomAppBar(
+          color: Colors.blueGrey,
           elevation: 5,
           child: IconTheme(
             data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
@@ -19,42 +26,40 @@ class Footer extends StatelessWidget {
                 Padding(
                   padding: const EdgeInsets.only(left: 15.0),
                   child: RichText(
-                      text: const TextSpan(children: [
-                    TextSpan(
-                      text: "© 2023 Helio Lunares | Coded with ",
-                    ),
-                    WidgetSpan(
+                      text: TextSpan(children: [
+                    footerText("© 2023 Helio Lunares | Coded with "),
+                    const WidgetSpan(
                       alignment: PlaceholderAlignment.middle,
                       child: Icon(
                         CupertinoIcons.heart_solid,
                         size: 18,
-                        color: Colors.black,
+                        color: Colors.white,
                       ),
                     ),
-                    TextSpan(text: " for "),
-                    WidgetSpan(
+                    footerText(" for "),
+                    const WidgetSpan(
                       alignment: PlaceholderAlignment.middle,
                       child: Icon(
                         Icons.apple,
                         size: 20,
-                        color: Colors.black,
+                        color: Colors.white,
                       ),
                     ),
-                    WidgetSpan(
+                    const WidgetSpan(
                       alignment: PlaceholderAlignment.middle,
                       child: Icon(
                         Icons.android,
                         size: 20,
-                        color: Colors.black,
+                        color: Colors.white,
                       ),
                     ),
-                    TextSpan(text: " & "),
-                    WidgetSpan(
+                    footerText(" & "),
+                    const WidgetSpan(
                       alignment: PlaceholderAlignment.middle,
                       child: Icon(
                         Icons.public,
                         size: 20,
-                        color: Colors.black,
+                        color: Colors.white,
                       ),
                     ),
                   ])),
@@ -70,12 +75,12 @@ class Footer extends StatelessWidget {
                                 Uri.parse("https://github.com/")),
                             tooltip: "Terms & Conditions",
                             icon: Icon(CupertinoIcons.doc),
-                            color: Colors.grey),
+                            color: Colors.white),
                         IconButton(
                           onPressed: () => print("HI"),
                           tooltip: "Licenses",
                           icon: Icon(Icons.code),
-                          color: Colors.grey,
+                          color: Colors.white,
                         ),
                       ],
                     ))
