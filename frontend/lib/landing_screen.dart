a15168c59c94e625af73072c6e3a1fbef9934cb7
Israfeel Ashraf <israfeel.ashraf@kcl.ac.uk>
2023-02-15 12:15:41 +0000

    Add note for landing screen designer

--- frontend/lib/landing_screen.dart
+++ frontend/lib/landing_screen.dart
@@ -2,6 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+/*
+THIS IS THE SCREEN THAT ALLOWS THE USER TO NAVIGATE TO EITHER LOGIN OR SIGN UP
+I THINK MAYBE JOSH IS DESIGNING THIS?
+
+FOR NOW, THE MAIN DART FILE JUST LINKS TO THE LOG IN SCREEN, BUT NEEDS TO BE CHANGED.
+
+*/
+
 class LandingScreen extends StatelessWidget {
   const LandingScreen({super.key});
 
