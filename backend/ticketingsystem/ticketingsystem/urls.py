87b4896cbdb16979bb766cb0e66f8ad43082235e
NDimish <nathanmani@outlook.com>
2023-02-16 18:35:33 +0000

    access both sides

--- backend/ticketingsystem/ticketingsystem/urls.py
+++ backend/ticketingsystem/ticketingsystem/urls.py
@@ -13,8 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+"""below import for testint"""
+
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', include('ticketing_api.urls')),
 ]
