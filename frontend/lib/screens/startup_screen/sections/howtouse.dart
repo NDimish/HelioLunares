b4026feeb0657bd0922eab964a654b731d2889be
Josh <joshuasusak@gmail.com>
2023-02-19 11:15:43 +0000

    feat: Create how to use section

--- frontend/lib/screens/startup_screen/sections/howtouse.dart
+++ frontend/lib/screens/startup_screen/sections/howtouse.dart
@@ -10,12 +10,53 @@ class HowToUseSection extends StatefulWidget {
 }
 
 class _HowToUseSectionState extends State<HowToUseSection> {
+  bool studentPage = false;
+  final ButtonStyle buttonStyle =
+      ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Column(children: [
-        const Text("how to use section"),
-      ]),
+      child: Center(
+          child: SizedBox(
+              width: MediaQuery.of(context).size.width / 2,
+              height: 700,
+              child: Card(
+                  elevation: 0,
+                  color: Color(0xFFffd1da).withOpacity(0.55),
+                  child: Center(
+                      child: Padding(
+                          padding: EdgeInsets.only(top: 10, bottom: 15),
+                          child: Column(
+                            children: [
+                              const Text("How to Use"),
+                              const Divider(),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  ElevatedButton(
+                                      style: buttonStyle,
+                                      onPressed: studentPage
+                                          ? () => setState(() {
+                                                studentPage = false;
+                                              })
+                                          : null,
+                                      child: const Text("For Students")),
+                                  ElevatedButton(
+                                      style: buttonStyle,
+                                      onPressed: !studentPage
+                                          ? () => setState(() {
+                                                studentPage = true;
+                                              })
+                                          : null,
+                                      child: const Text("For Societies")),
+                                ],
+                              ),
+                              !studentPage
+                                  ? const Text("Student Guide")
+                                  : const Text("Society Guide")
+                            ],
+                          )))))),
     );
   }
 }
