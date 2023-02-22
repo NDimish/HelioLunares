e9260db3876dca5ec63258fa58bc024ddab90c9a
Jaidev Nileshkumar K21054236 <jaidev.nileshkumar@kcl.ac.uk>
2023-02-22 10:39:45 +0000

    tests for user list for ordering and filtering

--- backend/api/tests/views/test_api_users.py
+++ backend/api/tests/views/test_api_users.py
@@ -65,3 +65,60 @@ class UsersTestCase(APITestCase):
         self.assertEquals(response.data[0]['email'],'johndoe@example.org')
         self.assertEquals(response.data[1]['email'],'janedoe@example.org')
         self.assertEquals(response.data[2]['email'],'jamesdoe@example.org')
+    
+    def test_url_outputs_with_invalid_filtering_type(self):
+        response = self.client.post('/log_in/',self.user_data,format='json')
+        self.assertNotEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+        response = self.client.get(self.url+'?InvalidFilter=Invalid', self.data, format='json')
+        self.assertEqual(len(response.data), 3)
+        self.assertEquals(response.data[0]['email'],'johndoe@example.org')
+        self.assertEquals(response.data[1]['email'],'janedoe@example.org')
+        self.assertEquals(response.data[2]['email'],'jamesdoe@example.org')
+    
+    def test_url_outputs_with_valid_filtering_type_and_invalid_filter(self):
+        response = self.client.post('/log_in/',self.user_data,format='json')
+        self.assertNotEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+        response = self.client.get(self.url+'?email=Invalid', self.data, format='json')
+        self.assertEqual(len(response.data), 0)
+    
+    def test_url_outputs_with_valid_filtering_type_and_valid_filter(self):
+        response = self.client.post('/log_in/',self.user_data,format='json')
+        self.assertNotEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+        response = self.client.get(self.url+'?email=johndoe@example.org', self.data, format='json')
+        self.assertEqual(len(response.data), 1)
+        self.assertEquals(response.data[0]['email'],'johndoe@example.org')
+    
+    def test_url_output_with_invalid_ordering_and_invalid_filter_type(self):
+        response = self.client.post('/log_in/',self.user_data,format='json')
+        self.assertNotEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+        response = self.client.get(self.url+'?ordering=InvalidOrderingType&InvalidFilterType=Invalid', self.data, format='json')
+        self.assertEqual(len(response.data), 3)
+        self.assertEquals(response.data[0]['email'],'johndoe@example.org')
+        self.assertEquals(response.data[1]['email'],'janedoe@example.org')
+        self.assertEquals(response.data[2]['email'],'jamesdoe@example.org')
+    
+    def test_url_output_with_valid_ordering_and_invalid_filter_type(self):
+        response = self.client.post('/log_in/',self.user_data,format='json')
+        self.assertNotEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+        response = self.client.get(self.url+'?ordering=email&InvalidFilterType=Invalid', self.data, format='json')
+        self.assertEqual(len(response.data), 3)
+        self.assertEquals(response.data[0]['email'],'jamesdoe@example.org')
+        self.assertEquals(response.data[1]['email'],'janedoe@example.org')
+        self.assertEquals(response.data[2]['email'],'johndoe@example.org')
+    
+    def test_url_output_with_invalid_ordering_and_valid_filter_type(self):
+        response = self.client.post('/log_in/',self.user_data,format='json')
+        self.assertNotEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+        response = self.client.get(self.url+'?ordering=InvalidOrderingType&email=jamesdoe@example.org', self.data, format='json')
+        self.assertEqual(len(response.data), 1)
+        self.assertEquals(response.data[0]['email'],'jamesdoe@example.org')
+    
+    def test_url_output_with_valid_ordering_and_valid_filter_type(self):
+        response = self.client.post('/log_in/',self.user_data,format='json')
+        self.assertNotEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+        response = self.client.get(self.url+'?ordering=email&user_level=1', self.data, format='json')
+        self.assertEqual(len(response.data), 3)
+        self.assertEquals(response.data[0]['email'],'jamesdoe@example.org')
+        self.assertEquals(response.data[1]['email'],'janedoe@example.org')
+        self.assertEquals(response.data[2]['email'],'johndoe@example.org')
+    
