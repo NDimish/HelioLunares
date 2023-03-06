b603378bb4273417b64dfca79d0dbf5ed7280e79
SitaSeth <k21034097@kcl.ac.uk>
2023-03-06 17:14:32 +0000

    Create buttons for edit page

--- frontend/lib/pages/edit_mode/edit_mode.dart
+++ frontend/lib/pages/edit_mode/edit_mode.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:university_ticketing_system/constants/controllers.dart';
+import 'package:university_ticketing_system/constants/style.dart';
 import 'package:university_ticketing_system/widgets/custom_text.dart';
 
 import '../../helpers/responsiveness.dart';
@@ -11,6 +12,7 @@ class EditModePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      //crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Obx(() => Row(
               children: [
@@ -22,6 +24,70 @@ class EditModePage extends StatelessWidget {
                     size: 24,
                     weight: FontWeight.bold,
                   ),
+                )
+              ],
+            )),
+        Flexible(
+            child: Container(
+          height: 50,
+        )),
+        Expanded(
+            flex: 10,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  flex: 20,
+                  child: ElevatedButton(
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: const BorderSide(
+                                  color: MyColours.elementButtonColour)),
+                        ),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            MyColours.elementButtonHoverColour),
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                            MyColours.panelBackgroundColour),
+                      ),
+                      // ignore: prefer_const_constructors
+                      child: CustomText(
+                        weight: FontWeight.w300,
+                        text: "Edit Hub Page Details",
+                        size: 20,
+                      ),
+                      onPressed: () {}),
+                ),
+                Flexible(
+                    child: Container(
+                  height: 10,
+                )),
+                Expanded(
+                  flex: 20,
+                  child: ElevatedButton(
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              // ignore: prefer_const_constructors
+                              side: BorderSide(
+                                  color: MyColours.elementButtonColour)),
+                        ),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            MyColours.elementButtonHoverColour),
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                            MyColours.panelBackgroundColour),
+                      ),
+                      // ignore: prefer_const_constructors
+                      child: CustomText(
+                        weight: FontWeight.w300,
+                        text: "Edit Society Details",
+                        size: 20,
+                      ),
+                      onPressed: () {}),
                 )
               ],
             ))
