ded8b71e6eb139001548c8852a9578953a93ad2f
Shakeeb Jumaan <shakeeb.jumaan@kcl.ac.uk>
2023-03-07 14:40:33 +0000

    Refactoring

--- frontend/lib/screens/user_settings/user_settings.dart
+++ frontend/lib/screens/user_settings/user_settings.dart
@@ -1,8 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:university_ticketing_system/screens/user_settings/widgets/settings_accpage.dart';
-import 'package:university_ticketing_system/screens/user_settings/widgets/settings_list.dart';
-import 'package:university_ticketing_system/screens/user_settings/widgets/settings_sidebar.dart';
+
 
 class UserSettingsPage extends StatefulWidget {
   const UserSettingsPage({super.key});
