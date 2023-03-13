222e09e50cca267337425cc8c1f541229f0ac9c1
SitaSeth <k21034097@kcl.ac.uk>
2023-03-13 21:38:31 +0000

    Add form validation

--- frontend/lib/pages/edit_mode/widgets/society_hub_form.dart
+++ frontend/lib/pages/edit_mode/widgets/society_hub_form.dart
@@ -45,6 +45,7 @@ class _SocietyHubFormState extends State<SocietyHubForm> {
       XFile? image = await _picker.pickImage(source: ImageSource.gallery);
       if (image != null) {
         var f = await image.readAsBytes();
+
         setState(() {
           _file = File("a");
           webImage = f;
@@ -83,7 +84,7 @@ class _SocietyHubFormState extends State<SocietyHubForm> {
           Expanded(
               child: SingleChildScrollView(
                   child: Column(children: [
-            TextField(
+            TextFormField(
               decoration: InputDecoration(
                   errorText:
                       _validateAboutUs ? 'About section Can\'t Be Empty' : null,
@@ -94,40 +95,51 @@ class _SocietyHubFormState extends State<SocietyHubForm> {
             const SizedBox(
               height: 20,
             ),
-            //IMAGE PICKER
-            ElevatedButton(
-                style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                        side: const BorderSide(
-                            color: MyColours.elementButtonColour)),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  //IMAGE PICKER
+                  ElevatedButton(
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                              side: const BorderSide(
+                                  color: MyColours.elementButtonColour)),
+                        ),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            MyColours.elementButtonHoverColour),
+                        foregroundColor: MaterialStateProperty.all<Color>(
+                            MyColours.panelBackgroundColour),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: const [
+                          Icon(Icons.image),
+                          SizedBox(
+                            width: 16,
+                          ),
+                          CustomText(
+                            weight: FontWeight.w300,
+                            text: "Select Image",
+                            size: 18,
+                          ),
+                        ],
+                      ),
+                      onPressed: () => pickImage()),
+                  const SizedBox(
+                    width: 10,
                   ),
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                      MyColours.elementButtonHoverColour),
-                  foregroundColor: MaterialStateProperty.all<Color>(
-                      MyColours.panelBackgroundColour),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Icon(Icons.image),
-                    SizedBox(
-                      width: 16,
-                    ),
-                    CustomText(
-                      weight: FontWeight.w300,
-                      text: "Select Image",
-                      size: 18,
-                    ),
-                  ],
-                ),
-                onPressed: () => pickImage()),
-
+                  CustomText(
+                    size: 14,
+                    text: "(200x200 is preferred)",
+                  ),
+                ]),
             const SizedBox(
               height: 20,
             ),
-
             (_file.path == "zz")
                 ? const FlutterLogo(
                     size: 200,
@@ -138,7 +150,6 @@ class _SocietyHubFormState extends State<SocietyHubForm> {
             const SizedBox(
               height: 20,
             ),
-
             TextButton(
               style: ButtonStyle(
                 shape: MaterialStateProperty.all<RoundedRectangleBorder>(
@@ -159,14 +170,16 @@ class _SocietyHubFormState extends State<SocietyHubForm> {
                         : _validateAboutUs = false;
                   },
                 );
-                if (checkAllValidators()) test = aboutUsController.text;
+                if (checkAllValidators()) {
+                  test = aboutUsController.text;
 
-                print(test);
-                // Get.put(obj);
+                  print(test);
+                  // Get.put(obj);
 
-                navigationController.goBack();
+                  navigationController.goBack();
 
-                navigationController.refresh();
+                  navigationController.refresh();
+                }
               },
               child: const CustomText(
                 text: "Save",
