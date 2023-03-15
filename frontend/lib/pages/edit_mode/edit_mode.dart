90c032aeb1d369229d01c1e0c1bb79e625289698
SitaSeth <k21034097@kcl.ac.uk>
2023-03-15 16:07:35 +0000

    Change edit mode page to display edit societyhub page form

--- frontend/lib/pages/edit_mode/edit_mode.dart
+++ frontend/lib/pages/edit_mode/edit_mode.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:university_ticketing_system/constants/controllers.dart';
-import 'package:university_ticketing_system/constants/style.dart';
-import 'package:university_ticketing_system/routing/routes.dart';
+import 'package:university_ticketing_system/pages/edit_mode/widgets/society_hub_form.dart';
 import 'package:university_ticketing_system/widgets/custom_text.dart';
 
 import '../../helpers/responsiveness.dart';
@@ -13,7 +12,6 @@ class EditModePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      //crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Obx(() => Row(
               children: [
@@ -28,77 +26,7 @@ class EditModePage extends StatelessWidget {
                 )
               ],
             )),
-        Flexible(
-            child: Container(
-          height: 50,
-        )),
-        Expanded(
-            flex: 10,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  flex: 20,
-                  child: ElevatedButton(
-                      style: ButtonStyle(
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18.0),
-                              side: const BorderSide(
-                                  color: MyColours.elementButtonColour)),
-                        ),
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            MyColours.elementButtonHoverColour),
-                        foregroundColor: MaterialStateProperty.all<Color>(
-                            MyColours.panelBackgroundColour),
-                      ),
-                      // ignore: prefer_const_constructors
-                      child: CustomText(
-                        weight: FontWeight.w300,
-                        text: "Edit Hub Page Details",
-                        size: 20,
-                      ),
-                      onPressed: () {
-                        navigationController
-                            .navigateTo(editSocietyHubPageDisplayName);
-                      }),
-                ),
-                Flexible(
-                    child: Container(
-                  height: 20,
-                )),
-                Expanded(
-                  flex: 20,
-                  child: ElevatedButton(
-                      style: ButtonStyle(
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18.0),
-                              // ignore: prefer_const_constructors
-                              side: BorderSide(
-                                  color: MyColours.elementButtonColour)),
-                        ),
-                        backgroundColor: MaterialStateProperty.all<Color>(
-                            MyColours.elementButtonHoverColour),
-                        foregroundColor: MaterialStateProperty.all<Color>(
-                            MyColours.panelBackgroundColour),
-                      ),
-                      // ignore: prefer_const_constructors
-                      child: CustomText(
-                        weight: FontWeight.w300,
-                        text: "Edit Society Details",
-                        size: 20,
-                      ),
-                      onPressed: () {}),
-                ),
-                Flexible(
-                    child: Container(
-                  height: 5,
-                )),
-              ],
-            ))
+        const Flexible(child: SocietyHubForm())
       ],
     );
   }
