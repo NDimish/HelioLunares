9fdd6dd3083122fbc52ca4d03bed8b3755a593d0
SitaSeth <k21034097@kcl.ac.uk>
2023-03-19 05:32:13 +0000

    Update test data

--- frontend/lib/pages/events/widgets/society_events.dart
+++ frontend/lib/pages/events/widgets/society_events.dart
@@ -14,9 +14,9 @@ class SocietyEventCards extends StatelessWidget {
   buildList() {
     SocietyEvent e = SocietyEvent(
         "Event One",
-        "3",
+        "3.50",
         formatter.format(DateTime.now()).toString(),
-        "Waterloo",
+        "Waterloo FWB B5",
         "1",
         //500 character example
         "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibu");
