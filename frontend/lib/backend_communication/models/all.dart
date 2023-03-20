8335fd5e4687745227edc8d7f0fbb8618232ed47
NDimish <nathanmani@outlook.com>
2023-03-20 17:22:53 +0000

    new role model added

--- frontend/lib/backend_communication/models/all.dart
+++ frontend/lib/backend_communication/models/all.dart
@@ -4,7 +4,7 @@ export 'People.dart';
 export '../models/society.dart';
 export 'User.dart';
 
-final DATASOURCE = 'http://127.0.0.1:8000/';
+final DATASOURCE = 'http://heliolunares.pythonanywhere.com/';
 
 class Localdata {
   String Token = "";
