e74bc9437b0b45630d2fa84ae0f26a1deb6104b3
jnile <k21054236@kcl.ac.uk>
2023-03-20 23:09:17 +0000

    remove unused code

--- frontend/lib/backend_communication/models/all.dart
+++ frontend/lib/backend_communication/models/all.dart
@@ -1,21 +1,8 @@
 export '../models/datasets.dart';
 export 'Event.dart';
 export 'People.dart';
-export '../models/society.dart';
+export 'Society.dart';
 export 'User.dart';
 export 'societyRole.dart';
 
 final DATASOURCE = 'http://localhost:8000/';
-
-class Localdata {
-  String Token = "";
-
-  String Email = '';
-
-  Localdata() {}
-
-  void setData(String token, String email) {
-    Token = token;
-    Email = email;
-  }
-}
