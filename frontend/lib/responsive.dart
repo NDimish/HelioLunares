48548661ccd7796428790d6cec7e86fbedfc7982
Israfeel Ashraf <israfeel.ashraf@kcl.ac.uk>
2023-03-20 21:35:13 +0000

    Add gradient animation, responsive design and style widgets

---
+++ frontend/lib/responsive.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+/// DESIGNED BY ISRAFEEL ASHRAF - K21008936
+///
+/// This responsive widget was used to enable responsive design of the application.
+/// It is used to check the screen sizes and then style the pages accordingly.
+///
+class ResponsiveWidget extends StatelessWidget {
+  final Widget largeScreen;
+  final Widget? mediumScreen;
+  final Widget? smallScreen;
+
+  const ResponsiveWidget({
+    Key? key,
+    required this.largeScreen,
+    this.mediumScreen,
+    this.smallScreen,
+  }) : super(key: key);
+
+  static bool isSmallScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width < 600;
+  }
+
+  static bool isLargeScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width > 1200;
+  }
+
+  static bool isMediumScreen(BuildContext context) {
+    return MediaQuery.of(context).size.width >= 600 &&
+        MediaQuery.of(context).size.width <= 1200;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 1200) {
+          return largeScreen;
+        } else if (constraints.maxWidth <= 1200 &&
+            constraints.maxWidth >= 600) {
+          return mediumScreen ?? largeScreen;
+        } else {
+          return smallScreen ?? largeScreen;
+        }
+      },
+    );
+  }
+}
