5d3f8cdaa4b1bc75dfc598a8a7d93e40675ff73c
SitaSeth <k21034097@kcl.ac.uk>
2023-03-21 09:53:53 +0000

    Remove obsolete import stmnt

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:university_ticketing_system/constants/style.dart';
-import 'package:university_ticketing_system/pages/select_society/select_society_layout.dart';
 import 'package:university_ticketing_system/pages/select_society/society_list_maker.dart';
 import 'package:university_ticketing_system/screens/landing_screen.dart';
 import 'package:get/get.dart';
