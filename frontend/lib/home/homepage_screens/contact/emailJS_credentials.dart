26253e20bd1bdf43617ad7ea93309a216863ec1d
Josh <joshuasusak@gmail.com>
2023-03-21 09:12:53 +0000

    feat: Add emailJS integration to contact form
    - Devs should enter a set of emailJS credentials.
    - If not entered, form will not send.

---
+++ frontend/lib/home/homepage_screens/contact/emailJS_credentials.dart
@@ -0,0 +1,8 @@
+//This is where you should paste your credentials, after creating your EmailJS account.
+
+class EmailJSCredentials {
+  static const String email_js_public_key = '';
+  static const String email_js_private_key = '';
+  static const String email_js_template = '';
+  static const String email_js_service = '';
+}
