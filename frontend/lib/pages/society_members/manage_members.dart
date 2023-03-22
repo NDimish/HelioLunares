c4062635e59ca1d348a24a59aa44e7918adcbc6c
jnile <jaidevnileshkumar2002@gmail.com>
2023-03-22 00:03:42 +0000

    load members data into manage members

--- frontend/lib/pages/society_members/manage_members.dart
+++ frontend/lib/pages/society_members/manage_members.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:university_ticketing_system/pages/society_members/members_table.dart';
+import 'package:university_ticketing_system/backend_communication/dataCollector.dart'
+    as data;
+import 'package:university_ticketing_system/globals.dart' as globals;
 
 class ManageMembers extends StatelessWidget {
   const ManageMembers({super.key});
   final int user_society_role_level = 3;
-  final int society_id = 1; // this is the user's role in society
-  final int user_level =
-      1; // this is user level, 1/2 for non/student and 3 for society account
+  final int society_id =
+      6; // this is the user's role in society // this is user level, 1/2 for non/student and 3 for society account
 
   @override
   Widget build(BuildContext context) {
@@ -17,23 +20,38 @@ class ManageMembers extends StatelessWidget {
             'You do not have enough power in this society (id=$society_id)!'),
       );
     } else if (user_society_role_level == 2 || user_society_role_level == 3) {
-      return Column(
-        children: [
-          const SizedBox(height: 40),
-          const Text("Admins"),
-          MembersTable(3, user_society_role_level, user_level),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text("Local Admins"),
-          MembersTable(2, user_society_role_level, user_level),
-          const SizedBox(
-            height: 20,
-          ),
-          const Text("Members"),
-          MembersTable(1, user_society_role_level, user_level),
-        ],
-      );
+      return MultiProvider(
+          providers: [
+            ChangeNotifierProvider(
+                create: (context) => data.dataCollector<data.SocietyRole>(
+                    filter: {'society': society_id.toString()},
+                    order: data.OrderType.CHRONOLOGICAL))
+          ],
+          builder: (context, child) {
+            final roleData =
+                Provider.of<data.dataCollector<data.SocietyRole>>(context);
+
+            return ListView(
+              children: [
+                const SizedBox(height: 40),
+                const Text("Admins"),
+                MembersTable(3, user_society_role_level,
+                    globals.localdataobj.getUserLevel(), roleData.collection),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text("Local Admins"),
+                MembersTable(2, user_society_role_level,
+                    globals.localdataobj.getUserLevel(), roleData.collection),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text("Members"),
+                MembersTable(1, user_society_role_level,
+                    globals.localdataobj.getUserLevel(), roleData.collection),
+              ],
+            );
+          });
     } else {
       return Center(
         child: Text('You have not joined this society (id=$society_id)'),
--- frontend/lib/pages/society_members/members_table.dart
+++ frontend/lib/pages/society_members/members_table.dart
@@ -2,14 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:university_ticketing_system/backend_communication/dataCollector.dart'
     as dataCol;
+import 'package:university_ticketing_system/backend_communication/models/all.dart';
 
 class MembersTable extends StatefulWidget {
   final int role;
   final int user_society_role_level; // this is the user's role in society
   final int
       user_level; // this is user level, 1/2 for non/student and 3 for society account
+  final List dataset;
 
-  const MembersTable(this.role, this.user_society_role_level, this.user_level,
+  const MembersTable(
+      this.role, this.user_society_role_level, this.user_level, this.dataset,
       {super.key});
 
   @override
@@ -71,7 +74,7 @@ class _MembersTableState extends State<MembersTable> {
     return const [];
   }
 
-  List<TableRow> _setData(List<dataCol.SocietyRole> roleCol) {
+  List<TableRow> _setData() {
     List<TableRow> dataArr = [
       TableRow(
           decoration: const BoxDecoration(
@@ -124,13 +127,16 @@ class _MembersTableState extends State<MembersTable> {
     ];
 
     //Simple way to add data to the tables
-    for (var i = 0; i < roleCol.length; i++) {
-      dataArr.add(_createDataRow({
-        "first_name": roleCol[i].people.first_name,
-        "last_name": roleCol[i].people.last_name,
-        "email": roleCol[i].people.user.email,
-        "student": (roleCol[i].people.user.userType == 2) ? 'True' : 'False',
-      }));
+    for (var i = 0; i < widget.dataset.length; i++) {
+      if (widget.dataset[i].role == widget.role) {
+        dataArr.add(_createDataRow({
+          "first_name": widget.dataset[i].people.first_name,
+          "last_name": widget.dataset[i].people.last_name,
+          "email": widget.dataset[i].people.user.email,
+          "student":
+              (widget.dataset[i].people.user.userType == 2) ? 'True' : 'False',
+        }));
+      }
     }
 
     return dataArr;
@@ -171,26 +177,14 @@ class _MembersTableState extends State<MembersTable> {
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-              create: (context) => dataCol.dataCollector<dataCol.SocietyRole>(
-                  filter: {}, order: dataCol.OrderType.CHRONOLOGICAL))
-        ],
-        builder: (context, child) {
-          final rolesData =
-              Provider.of<dataCol.dataCollector<dataCol.SocietyRole>>(context);
-          print(rolesData.collection);
-          return Table(
-            border: const TableBorder(
-                top: BorderSide(width: 2, color: Color(0xFF703c6c)),
-                bottom: BorderSide(width: 2, color: Color(0xFF703c6c)),
-                right: BorderSide(width: 2, color: Color(0xFF703c6c)),
-                left: BorderSide(width: 2, color: Color(0xFF703c6c)),
-                horizontalInside:
-                    BorderSide(width: .5, color: Color(0xFF703c6c))),
-            children: _setData(rolesData.collection),
-          );
-        });
+    return Table(
+      border: const TableBorder(
+          top: BorderSide(width: 2, color: Color(0xFF703c6c)),
+          bottom: BorderSide(width: 2, color: Color(0xFF703c6c)),
+          right: BorderSide(width: 2, color: Color(0xFF703c6c)),
+          left: BorderSide(width: 2, color: Color(0xFF703c6c)),
+          horizontalInside: BorderSide(width: .5, color: Color(0xFF703c6c))),
+      children: _setData(),
+    );
   }
 }
