8351a4dab014d3b5a19fedf4ba0646be3dbff081
jnile <jaidevnileshkumar2002@gmail.com>
2023-03-22 22:26:48 +0000

    set people account to access their selected soc

--- frontend/lib/pages/society_members/manage_members.dart
+++ frontend/lib/pages/society_members/manage_members.dart
@@ -7,83 +7,105 @@ import 'package:university_ticketing_system/backend_communication/dataCollector.
     as data;
 import 'package:university_ticketing_system/globals.dart' as globals;
 
-class ManageMembers extends StatelessWidget {
+class ManageMembers extends StatefulWidget {
   final int societyId;
   const ManageMembers({required this.societyId, super.key});
+
+  @override
+  State<ManageMembers> createState() => _ManageMembersState();
+}
+
+class _ManageMembersState extends State<ManageMembers> {
+  var x = 0;
+  var roleData;
   // this is the user's role in society // this is user level, 1/2 for non/student and 3 for society account
+  void callback(int test) {
+    setState(() {
+      print("YOOO WHAT IS THIS");
+      print(roleData.collection[0].role);
+      this.x = test;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     int userSocietyRoleLevel = 0;
-    print(Get.find<data.Society>());
-    return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-              create: (context) =>
-                  data.dataCollector<data.SocietyRole>(filter: {
-                    'society': societyId.toString(),
-                  }, order: data.OrderType.CHRONOLOGICAL))
-        ],
-        builder: (context, child) {
-          final roleData =
-              Provider.of<data.dataCollector<data.SocietyRole>>(context);
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: MultiProvider(
+          providers: [
+            ChangeNotifierProvider(
+                create: (context) =>
+                    data.dataCollector<data.SocietyRole>(filter: {
+                      'society': widget.societyId.toString(),
+                    }, order: data.OrderType.CHRONOLOGICAL))
+          ],
+          builder: (context, child) {
+            roleData =
+                Provider.of<data.dataCollector<data.SocietyRole>>(context);
+            print("Test1");
+            print(roleData.collection[0].role);
+            print("Test2");
+            //now check user role level first
+            if (globals.localdataobj.getToken() == "") {
+              return const Text("You have not been authenticated.");
+            }
 
-          //now check user role level first
-          if (globals.localdataobj.getToken() == "") {
-            return const Text("You have not been authenticated.");
-          }
-
-          if (globals.localdataobj.getUserLevel() != 3) {
-            //check in the array if they are in soc and have power
-            for (var i = 0; i < roleData.collection.length; i++) {
-              if (roleData.collection[i].people.user.id ==
-                  globals.localdataobj.getUserID()) {
-                userSocietyRoleLevel = roleData.collection[i].role;
-                break;
+            if (globals.localdataobj.getUserLevel() != 3) {
+              //check in the array if they are in soc and have power
+              for (var i = 0; i < roleData.collection.length; i++) {
+                if (roleData.collection[i].people.user.id ==
+                    globals.localdataobj.getUserID()) {
+                  userSocietyRoleLevel = roleData.collection[i].role;
+                  break;
+                }
+              }
+              if (userSocietyRoleLevel == 1) {
+                return const Text("You do not have power in society.");
+              }
+              if (userSocietyRoleLevel == 0) {
+                return const Text("You are not part of this society.");
               }
             }
-            if (userSocietyRoleLevel == 1) {
-              return const Text("You do not have power in society.");
-            }
-            if (userSocietyRoleLevel == 0) {
-              return const Text("You are not part of this society.");
-            }
-          }
 
-          //They have enough power to be in this society management
+            //They have enough power to be in this society management
 
-          return ListView(
-            children: [
-              const SizedBox(height: 40),
-              const Text("Admins"),
-              MembersTable(
-                  3,
-                  userSocietyRoleLevel,
-                  globals.localdataobj.getUserLevel(),
-                  societyId,
-                  roleData.collection),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text("Local Admins"),
-              MembersTable(
-                  2,
-                  userSocietyRoleLevel,
-                  globals.localdataobj.getUserLevel(),
-                  societyId,
-                  roleData.collection),
-              const SizedBox(
-                height: 20,
-              ),
-              const Text("Members"),
-              MembersTable(
-                  1,
-                  userSocietyRoleLevel,
-                  globals.localdataobj.getUserLevel(),
-                  societyId,
-                  roleData.collection),
-            ],
-          );
-        });
+            return ListView(
+              children: [
+                const SizedBox(height: 40),
+                const Text("Admins"),
+                MembersTable(
+                    3,
+                    userSocietyRoleLevel,
+                    globals.localdataobj.getUserLevel(),
+                    widget.societyId,
+                    roleData.collection,
+                    callback),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text("Local Admins"),
+                MembersTable(
+                    2,
+                    userSocietyRoleLevel,
+                    globals.localdataobj.getUserLevel(),
+                    widget.societyId,
+                    roleData.collection,
+                    callback),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text("Members"),
+                MembersTable(
+                    1,
+                    userSocietyRoleLevel,
+                    globals.localdataobj.getUserLevel(),
+                    widget.societyId,
+                    roleData.collection,
+                    callback),
+              ],
+            );
+          }),
+    );
   }
 }
--- frontend/lib/pages/society_members/society_members.dart
+++ frontend/lib/pages/society_members/society_members.dart
@@ -34,7 +34,7 @@ class SocietyMembersPage extends StatelessWidget {
             child: (globals.localdataobj.getUserLevel() == 3)
                 ? const ManageMembersWrapper()
                 : //Put society id here from when the user selects it
-                ManageMembers(societyId: 6))
+                ManageMembers(societyId: Get.find<data.Society>().id))
       ],
     );
   }
