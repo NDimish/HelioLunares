99fc8712e79342f5f39bf9aaa5ee29b86a0b99f4
jnile <jaidevnileshkumar2002@gmail.com>
2023-03-23 19:02:42 +0000

    Add attendance button

--- frontend/lib/pages/statistics/StatisticsPage.dart
+++ frontend/lib/pages/statistics/StatisticsPage.dart
@@ -15,6 +15,7 @@ import 'package:university_ticketing_system/backend_communication/models/Event.d
 import 'package:university_ticketing_system/backend_communication/models/SocietyRole.dart';
 
 import 'package:university_ticketing_system/globals.dart' as globals;
+import 'package:university_ticketing_system/pages/statistics/set_attendance.dart';
 
 class Statistics extends StatefulWidget {
   final int societyId;
@@ -25,6 +26,87 @@ class Statistics extends StatefulWidget {
 }
 
 class _StatisticsState extends State<Statistics> {
+  void callback() {
+    setState(() {});
+  }
+
+  final TextEditingController _textFieldController = TextEditingController();
+  String? codeDialog;
+  String? valueText;
+
+  Future<void> _updateAttendance(int eventId) async {
+    print("This is updating attendance.");
+    // Event=${widget.societyId}
+    final response = await http.put(
+        Uri.parse("${globals.DATASOURCE}event/${eventId}/"),
+        headers: (globals.localdataobj.getToken() != "")
+            ? {
+                HttpHeaders.authorizationHeader:
+                    "token ${globals.localdataobj.getToken()}"
+              }
+            : {},
+        body: {'attendance': codeDialog});
+    if (response.statusCode == 201) {
+      // print(response.body);
+      List data;
+      try {
+        data = json.decode(response.body) as List;
+      } catch (e) {
+        data = json.decode("[" + response.body + "]") as List;
+      }
+      return Future.delayed(const Duration(microseconds: 0),
+          () => {data.map<Event>((json) => (Event.fromJson(json))).toList()});
+    }
+    print("Finished updating");
+  }
+
+  Future<void> _displayTextInputDialog(
+      BuildContext context, int eventId) async {
+    //This function have been researched/reused from:
+    // https://www.appsdeveloperblog.com/alert-dialog-with-a-text-field-in-flutter/
+
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Add Attendace'),
+            content: TextField(
+              onChanged: (value) {
+                setState(() {
+                  valueText = value;
+                });
+              },
+              controller: _textFieldController,
+              decoration:
+                  const InputDecoration(hintText: "Enter attendance here..."),
+            ),
+            actions: <Widget>[
+              MaterialButton(
+                color: Colors.red,
+                textColor: Colors.white,
+                child: const Text('CANCEL'),
+                onPressed: () {
+                  setState(() {
+                    Navigator.pop(context);
+                  });
+                },
+              ),
+              MaterialButton(
+                color: Colors.green,
+                textColor: Colors.white,
+                child: const Text('OK'),
+                onPressed: () {
+                  codeDialog = valueText;
+                  _updateAttendance(eventId);
+                  Navigator.pop(context);
+                  callback();
+                },
+              ),
+            ],
+          );
+        });
+  }
+
   Future<Set<List<Event>>> _fetchEventData() async {
     print("This is loading event data.");
     // Event=${widget.societyId}
@@ -174,7 +256,10 @@ class _StatisticsState extends State<Statistics> {
                 child: Text("Get List of tickets"),
               ),
             ],
-            onChanged: ((value) => {}),
+            onChanged: ((value) => {
+                  if (value == "addattendance")
+                    {_displayTextInputDialog(context, items[i].id)},
+                }),
           ),
         )
       ]));
@@ -255,52 +340,46 @@ class _StatisticsState extends State<Statistics> {
                   child: _allEvents(snapshot.requireData.first),
                 ),
                 //This is the graph
-                Expanded(
-                  child: AspectRatio(
-                    aspectRatio: 1.50,
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: LineChart(LineChartData(
-                        gridData: FlGridData(
-                          show: true,
-                          drawVerticalLine: true,
-                          horizontalInterval: 1,
-                          verticalInterval: 1,
-                          getDrawingHorizontalLine: (value) {
-                            return FlLine(
-                              color: Color(0xFFFFFFFF),
-                              strokeWidth: 1,
-                            );
-                          },
-                          getDrawingVerticalLine: (value) {
-                            return FlLine(
-                              color: Color(0xFFFFFFFF),
-                              strokeWidth: 1,
-                            );
-                          },
-                        ),
-                        borderData: FlBorderData(
-                          show: true,
-                          border: Border.all(color: const Color(0xff37434d)),
-                        ),
-                        minX: 0,
-                        maxX: 5,
-                        minY: 0,
-                        maxY: 5,
-                        lineBarsData: [
-                          LineChartBarData(spots: const [
-                            FlSpot(0, 0),
-                            FlSpot(1, 2),
-                            FlSpot(2, 5),
-                            FlSpot(3, 3),
-                            FlSpot(4, 4),
-                            FlSpot(5, 3),
-                          ])
-                        ],
-                      )),
-                    ),
-                  ),
-                )
+                // Expanded(
+                //   child: AspectRatio(
+                //     aspectRatio: 1.50,
+                //     child: Padding(
+                //       padding: const EdgeInsets.all(20),
+                //       child: LineChart(LineChartData(
+                //         gridData: FlGridData(
+                //           show: true,
+                //           drawVerticalLine: true,
+                //           horizontalInterval: 1,
+                //           verticalInterval: 1,
+                //           getDrawingHorizontalLine: (value) {
+                //             return FlLine(
+                //               color: Color(0xFFFFFFFF),
+                //               strokeWidth: 1,
+                //             );
+                //           },
+                //           getDrawingVerticalLine: (value) {
+                //             return FlLine(
+                //               color: Color(0xFFFFFFFF),
+                //               strokeWidth: 1,
+                //             );
+                //           },
+                //         ),
+                //         borderData: FlBorderData(
+                //           show: true,
+                //           border: Border.all(color: const Color(0xff37434d)),
+                //         ),
+                //         // minX: 0,
+                //         // maxX: 5,
+                //         // minY: 0,
+                //         // maxY: 5,
+                //         lineBarsData: [
+                //           LineChartBarData(
+                //               spots: _getGraphData(snapshot.requireData.first))
+                //         ],
+                //       )),
+                //     ),
+                //   ),
+                // )
               ],
             );
           } else {
