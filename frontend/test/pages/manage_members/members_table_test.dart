e6764c1fb9c0035cf2e47decedd748c2ab644334
jnile <jaidevnileshkumar2002@gmail.com>
2023-03-24 12:14:41 +0000

    attempt testing wihtout data

--- frontend/test/pages/manage_members/members_table_test.dart
+++ frontend/test/pages/manage_members/members_table_test.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
+import 'package:university_ticketing_system/backend_communication/models/University.dart';
 import 'package:university_ticketing_system/pages/society_members/members_table.dart';
 import 'package:university_ticketing_system/pages/society_members/society_members.dart';
+import 'package:university_ticketing_system/backend_communication/models/all.dart';
 import 'package:mockito/mockito.dart';
 import 'package:http/http.dart' as http;
 
@@ -11,15 +13,16 @@ void main() {
   final TestWidgetsFlutterBinding binding =
       TestWidgetsFlutterBinding.ensureInitialized();
 
-  group('Society Members', () {
+  group('Members Table', () {
     //Ensure screen size
     binding.window.physicalSizeTestValue = const Size(1920, 1080);
     binding.window.devicePixelRatioTestValue = 1.0;
 
     testWidgets('Widget renders', (tester) async {
       await tester.pumpWidget(MembersTable(3, 3, 3, 1, [], () => {}));
-      final notAuthFinder = find.text("Not Authenticated");
-      expect(notAuthFinder, findsOneWidget);
+      final findUserID = find.text("ID");
+
+      expect(findUserID, findsOneWidget);
     });
   });
 }
